d2952e630cd17fdf50316361fefc2a2507a9960d
lois.alberto <lois.alberto@Workstation-40.cmphil.calcmenu.com>
2021-01-08 13:49:37 +0800

    Add User and Edit User Test script

--- src/Utility/Constant.java
+++ src/Utility/Constant.java
@@ -5,7 +5,7 @@ package Utility;
 
 	public class Constant {  
 		public static final String URL = "http://va-server02/CalcmenuWeb_Template_V91/index.aspx";
-		public static final String Path_TestData = "C:\\TestData\\";
+		public static final String Path_TestData = "\\\\egs\\Automation Project\\Selenium\\TestData\\";
 		public static final String File_TestData = "Automation_CMWeb_TestData.xlsx";
 		
 		//Test Data Sheet Columns
